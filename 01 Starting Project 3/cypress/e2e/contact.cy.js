322ace6cead943d97adebf733613243b47a00494
Greg <Greg@2book.com>
2023-02-23 14:49:05 -0500

    Section 03.39: Validate the form

--- 01 Starting Project 3/cypress/e2e/contact.cy.js
+++ 01 Starting Project 3/cypress/e2e/contact.cy.js
@@ -36,4 +36,15 @@ describe('contact form', () => {
             .contains('Sending...')
             .and('have.attr', 'disabled');
     });
+
+    it.only('should validate the form input', () => {
+        cy.get('[data-cy="contact-btn-submit"]')
+            .as('submitBtn')
+            .click();
+        cy.get('@submitBtn').then(el => {
+            expect(el).to.not.have.attr('disabled');
+            expect(el.text()).to.not.equal('Sending...');
+            expect(el.text()).to.equal('Send Message');
+        })
+    })
 });
