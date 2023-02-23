fddb6f94889d3251d5f6a730d92d880c6bc6b859
Greg <Greg@2book.com>
2023-02-23 15:07:54 -0500

    Section 03.41: Refactor w/ @alias

--- 01 Starting Project 3/cypress/e2e/contact.cy.js
+++ 01 Starting Project 3/cypress/e2e/contact.cy.js
@@ -1,71 +1,71 @@
 describe('contact form', () => {
     beforeEach(()=>{
         cy.visit('http://127.0.0.1:5173/about');
+
+        // ASSIGN ALIAS' TO INPUT EL's
+        cy.get('[data-cy="contact-input-message"]').as('inputMsg');
+        cy.get('[data-cy="contact-input-name"]').as('inputName');
+        cy.get('[data-cy="contact-input-email"]').as('inputEmail');
+        cy.get('[data-cy="contact-btn-submit"]').as('btnSubmit');
     });
 
     it('should submit the form by clicking the send button', () => {
-        cy.get('[data-cy="contact-input-message"]').type('Some message here');
-        cy.get('[data-cy="contact-input-name"]').type('John Doe');
-        cy.get('[data-cy="contact-input-email"]').type('test@example.com');
-        // cy.get('[data-cy="contact-btn-submit"]')
+        cy.get('@inputMsg').type('Some message here');
+        cy.get('@inputName').type('John Doe');
+        cy.get('@inputEmail').type('test@example.com');
+        // cy.get('@btnSubmit')
         //     .contains('Send Message')
         //     .and('not.have.attr', 'disabled');
-        cy.get('[data-cy="contact-btn-submit"]')
-            .as('submitBtn')
+        cy.get('@btnSubmit')
             .then(el => {
                 expect(el.text()).to.eq('Send Message');
                 expect(el.attr('disabled')).to.be.undefined;
             });
-        cy.get('@submitBtn').click();
-        cy.get('@submitBtn')
+        cy.get('@btnSubmit').click();
+        cy.get('@btnSubmit')
             .contains('Sending...')
             .and('have.attr', 'disabled');
     });
 
     it('should submit the form by user pressing {enter}', () => {
-        cy.get('[data-cy="contact-input-message"]').type('Some message here');
-        cy.get('[data-cy="contact-input-name"]').type('John Doe');
-        cy.get('[data-cy="contact-btn-submit"]')
-            .as('submitBtn')
+        cy.get('@inputMsg').type('Some message here');
+        cy.get('@inputName').type('John Doe');
+        cy.get('@btnSubmit')
             .then(el => {
                 expect(el.text()).to.eq('Send Message');
                 expect(el.attr('disabled')).to.be.undefined;
             });
-        cy.get('[data-cy="contact-input-email"]').type('test@example.com{enter}');
-        cy.get('@submitBtn')
+        cy.get('@inputEmail').type('test@example.com{enter}');
+        cy.get('@btnSubmit')
             .contains('Sending...')
             .and('have.attr', 'disabled');
     });
 
-    it.only('should validate the form input', () => {
-        cy.get('[data-cy="contact-btn-submit"]')
-            .as('submitBtn')
+    it('should validate the form input', () => {
+        cy.get('@btnSubmit')
             .click();
-        cy.get('@submitBtn').then(el => {
+        cy.get('@btnSubmit').then(el => {
             expect(el).to.not.have.attr('disabled');
             expect(el.text()).to.not.equal('Sending...');
             expect(el.text()).to.equal('Send Message');
         });
 
         // Check classes on blur
-        cy.get('[data-cy="contact-input-message"]')
-            .as('inputMsg')
+        cy.get('@inputMsg')
             .focus()
             .blur();
         cy.get('@inputMsg').parent().then(el => {
             expect(el.attr('class')).to.contains('invalid');
         });
 
-        cy.get('[data-cy="contact-input-name"]')
-            .as('inputName')
+        cy.get('@inputName')
             .focus()
             .blur();
         cy.get('@inputName').parent().then(el => {
             expect(el.attr('class')).to.contains('invalid');
         });
 
-        cy.get('[data-cy="contact-input-email"]')
-            .as('inputEmail')
+        cy.get('@inputEmail')
             .focus()
             .blur();
         cy.get('@inputEmail').parent().then(el => {
