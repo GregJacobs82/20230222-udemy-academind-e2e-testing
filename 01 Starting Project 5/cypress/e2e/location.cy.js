89cc49fc60de20c9db412032f4e8562dcf3e6e4c
Greg <Greg@2book.com>
2023-02-23 16:38:58 -0500

    Section 05.66: Fake Stub Implementation
    I don't like that he's using setTimeout here to simulate the response. Would prefer to use cy.intercept() to wait for something to happen, but whatever 🤷🏼‍♂️

--- 01 Starting Project 5/cypress/e2e/location.cy.js
+++ 01 Starting Project 5/cypress/e2e/location.cy.js
@@ -3,9 +3,22 @@
 describe('share location', () => {
     it('should fetch the user location', () => {
         cy.visit('/').then(win => {
-            cy.stub(win.navigator.geolocation, 'getCurrentPosition').as('getUserPosition');
+            cy.stub(win.navigator.geolocation, 'getCurrentPosition')
+                .as('getUserPosition')
+                .callsFake(cb => {
+                    setTimeout(() => { // todo: i don't like this setTimeout 👎🏼
+                        cb({
+                            coords: {
+                                latitude: '32.776474',
+                                longitude: '-79.931053',
+                            }
+                        });
+                    }, 100);
+                });
         });
         cy.get('[data-cy="get-loc-btn"]').click();
         cy.get('@getUserPosition').should('have.been.called');
+        cy.get('[data-cy="get-loc-btn"]').should('be.disabled');
+        cy.get('[data-cy="actions"]').should('contain', 'Location fetched');
     });
 });
