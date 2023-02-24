836f1bd18ff47fca53f74a353177b09add45267c
Greg <Greg@2book.com>
2023-02-24 16:21:03 -0500

    Section 06.88: Authorization w/ session cookie

---
+++ 01 Starting Project 6/cypress/e2e/auth.cy.js
@@ -0,0 +1,15 @@
+describe('Authorization', () => {
+    beforeEach(() => {
+        cy.task('seedDatabase');
+    });
+
+    it('should signup', () => {
+        cy.visit('/signup');
+        cy.get('[data-cy="auth-email"]').click();
+        cy.get('[data-cy="auth-email"]').type('test2@example.com');
+        cy.get('[data-cy="auth-password"]').type('testpassword');
+        cy.get('[data-cy="auth-submit"]').click();
+        cy.location('pathname').should('eq', '/takeaways');
+        cy.getCookie('__session').its('value').should('not.be.empty'); // VIDEO: https://www.udemy.com/course/cypress-end-to-end-testing-getting-started/learn/lecture/36409782
+    });
+});
