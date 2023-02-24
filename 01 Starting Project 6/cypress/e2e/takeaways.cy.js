b27bd5dc168aadb1d55ee54f56bdb5068d03a40d
Greg <Greg@2book.com>
2023-02-24 17:07:32 -0500

    Section 06.92: Create Takeaway - Intercept & check request.body

--- 01 Starting Project 6/cypress/e2e/takeaways.cy.js
+++ 01 Starting Project 6/cypress/e2e/takeaways.cy.js
@@ -9,4 +9,21 @@ describe('Takeaways', () => {
         cy.visit('/');
         cy.get('[data-cy="takeaway-item"]').should('have.length', 2);
     });
+
+    it.only('should add a new takeaway', () => {
+        cy.userLogin();
+        cy.intercept('POST', '/takeaways/new*', 'success').as('createTakeaway');
+        cy.contains('Add a new takeaway').click();
+        cy.location('pathname').should('eq', '/takeaways/new');
+        cy.get('[data-cy="title"]').click();
+        cy.get('[data-cy="title"]').type('TestTitle1');
+        cy.get('[data-cy="body"]').type('TestBody1');
+        cy.get('[data-cy="create-takeaway"]').click();
+        cy.wait('@createTakeaway')
+            .its('request.body')
+            .should('match', /TestTitle1.*TestBody1/)
+
+        // cy.location('pathname').should('eq', '/takeaways');
+        // cy.get('takeaway').should('have.length', 3);
+    })
 });
