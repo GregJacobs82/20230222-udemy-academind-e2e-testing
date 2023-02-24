b81ab739b39a0438d774c0f3ae5bf9391e5511ae
Greg <Greg@2book.com>
2023-02-24 15:59:15 -0500

    Section 06.85: Intercept w/ message response

--- 01 Starting Project 6/cypress/e2e/newsletter.cy.js
+++ 01 Starting Project 6/cypress/e2e/newsletter.cy.js
@@ -12,4 +12,14 @@ describe('Newsletter', () => {
         cy.wait('@subscribe')
         cy.contains('Thanks for signing up!');
     });
+
+    it('should display validation errors', () => {
+        cy.intercept('POST', '/newsletter*', { message: 'This email is already subscribed.' }) // Stop HTTP request & provide message response - VIDEO: https://www.udemy.com/course/cypress-end-to-end-testing-getting-started/learn/lecture/36409770
+            .as('subscribe');
+        cy.visit('/');
+        cy.get('[data-cy="newsletter-email"]').type('test@example.com');
+        cy.get('[data-cy="newsletter-submit"]').click();
+        cy.wait('@subscribe')
+        cy.contains('This email is already subscribed.');
+    });
 });
