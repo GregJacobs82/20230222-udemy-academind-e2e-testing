d51f56bccc0280a6ece2c989f5be654023dd7b51
Greg <Greg@2book.com>
2023-02-24 15:52:05 -0500

    Section 06.83: `cy.intercept()` w/ `cy.wait()`

--- 01 Starting Project 6/cypress/e2e/newsletter.cy.js
+++ 01 Starting Project 6/cypress/e2e/newsletter.cy.js
@@ -4,9 +4,12 @@ describe('Newsletter', () => {
     });
 
     it('should display success message', () => {
+        cy.intercept('POST', '/newsletter*', { status: 201 }) // intercept any HTTP request localhost:3000/newsletter?anything - VIDEO: https://www.udemy.com/course/cypress-end-to-end-testing-getting-started/learn/lecture/36409762
+            .as('subscribe');
         cy.visit('/');
         cy.get('[data-cy="newsletter-email"]').type('test@example.com');
         cy.get('[data-cy="newsletter-submit"]').click();
+        cy.wait('@subscribe')
         cy.contains('Thanks for signing up!');
     });
 });
