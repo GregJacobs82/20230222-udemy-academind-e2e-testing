af250a2c273fc9d9c8c3698e52f0fd3e0c178cbc
Greg <Greg@2book.com>
2023-02-24 16:37:45 -0500

    Section 06.91: Login Command

--- 01 Starting Project 6/cypress/e2e/auth.cy.js
+++ 01 Starting Project 6/cypress/e2e/auth.cy.js
@@ -13,13 +13,14 @@ describe('Authorization', () => {
         cy.getCookie('__session').its('value').should('not.be.empty'); // VIDEO: https://www.udemy.com/course/cypress-end-to-end-testing-getting-started/learn/lecture/36409782
     });
 
-    it.only('should login', () => {
-        cy.visit('/login');
-        cy.get('[data-cy="auth-email"]').click();
-        cy.get('[data-cy="auth-email"]').type('test@example.com');
-        cy.get('[data-cy="auth-password"]').type('testpassword');
-        cy.get('[data-cy="auth-submit"]').click();
-        cy.location('pathname').should('eq', '/takeaways');
-        cy.getCookie('__session').its('value').should('not.be.empty');
+    it('should login', () => {
+        cy.login();
+    });
+
+    it('should logout', () => {
+        cy.login();
+        cy.contains('Logout').click();
+        cy.location('pathname').should('eq', '/');
+        cy.getCookie('__session').its('value').should('be.empty');
     });
 });
